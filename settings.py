354020c9c7102aa2b61e27fd09faab1b7ad674fb
RedFantom <redfantom@outlook.com>
2017-06-11 14:07:14 +0200

    Add file for settings

---
+++ settings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2017 by RedFantom
+# For license see LICENSE
+
+# Originally written for the GSF Parser, modified for this program
+import collections
+import tkinter.messagebox
+import os
+import configparser
+import ast
+import utilities
+
+
+class Settings(object):
+
+    defaults = {
+
+    }
+
+    def __init__(self):
+        pass
