1f36fe419ab0cd78acaddf4a41d4b6ec882afba1
RedFantom <redfantom@outlook.com>
2017-06-11 14:07:21 +0200

    Add OCRLoop class

---
+++ ocr.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2017 by RedFantom
+# For license see LICENSE
+from threading import Thread
+from PIL import ImageGrab
+
+
+class OCRLoop(Thread):
+    def run(self):
+        pass
+
