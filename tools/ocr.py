443f15d43bb59088ca22dd6d571b4c5dd193764c
RedFantom <redfantom@outlook.com>
2017-07-04 22:16:59 +0200

    Added some OCR code

--- tools/ocr.py
+++ tools/ocr.py
@@ -4,6 +4,8 @@
 # For license see LICENSE
 from threading import Thread
 from PIL import ImageGrab
+from guiparser.guiparser import GUIParser
+from simpleocr import OCR, enhance_image
 
 
 class OCRLoop(Thread):
@@ -11,7 +13,22 @@ class OCRLoop(Thread):
         Thread.__init__(self)
         self.callback = callback
         self.exit_queue = exitq
+        self.flag = True
+        self.ocr = OCR()
 
     def run(self):
+        while self.flag:
+            # Get the chat box position
+            image = ImageGrab.grab()
+            image = enhance_image(image, color=0.0, contrast=2.0, sharpness=2.0)
+            text, _, _ = self.ocr.ocr(image)
+            if callable(self.callback):
+                self.callback(text)
+            continue
+
+    def train(self):
         pass
+
+    def close(self):
+        self.flag = False
 
